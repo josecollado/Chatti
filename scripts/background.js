4a90f6bafa855067184bc986e73e893a9b28e138
Jose Collado <jcoll1020@gmail.com>
2023-10-03 15:38:22 -0400

    working on background service

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,10 @@
+let keywords;
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Chrome Extenstion Installed Successfully')
 })
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    keywords = message.action
+});
+
--- scripts/popup.js
+++ scripts/popup.js
@@ -2,5 +2,6 @@ $(document).ready(()=>{
     let keywords = '';
     $('.form-control').on("keyup", () =>{
         keywords = $('.form-control').val()
+        chrome.runtime.sendMessage({ action: keywords });
     })
 })

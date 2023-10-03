aa939bdf4114b79c85e51c70baf9c01194b9d6bb
Jose Collado <jcoll1020@gmail.com>
2023-10-03 13:13:46 -0400

    downloaded files loacally, worked on getting the keyword values using jquery, set up background service and took care of content setting errors the tab was giving me

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,2 +1,10 @@
-const topDraftDiv = document.getElementsByClassName('public-DraftStyleDefault-block')[0]
+// const feedPageDraftRow = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-18u37iz r-1s2bzr4")
+// const draftDiv = document.getElementsByClassName("public-DraftStyleDefault-block public-DraftStyleDefault-ltr")[0]
 
+
+// const iconNode = document.createElement('i')
+// const iconDiv = document.createElement('div')
+// iconDiv.append(iconNode)
+// iconNode.classList.add('bi', 'bi-chat-dots-fill')
+
+// // console.log(feedPageDraftRow)
---
+++ scripts/popup.js
@@ -0,0 +1,6 @@
+$(document).ready(()=>{
+    let keywords = '';
+    $('.form-control').on("keyup", () =>{
+        keywords = $('.form-control').val()
+    })
+})
